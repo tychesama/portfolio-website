17658e7ec131e09e74a267c45a43758edeb9b268
Jose Emmanuel R. Idpan <jridpan1225@gmail.com>
2025-08-24 18:19:43 +0800

    unused imports and variable fix
    deployment bruh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import SplashScreen from './components/SplashScreen';
+import {  } from 'react';
+// import SplashScreen from './components/SplashScreen';
 import MainPage from './pages/MainPage';
-import ThemeSwitcher from './components/ThemeSwitcher';
+// import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
+  // const [isSplashVisible, setIsSplashVisible] = useState(true);
 
   return (
     <>
